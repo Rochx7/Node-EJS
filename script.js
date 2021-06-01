6e753e2a857fbc448dcbef6e7a54337cb99f0968
Guilherme Rocha <guirocha55@hotmail.com>
2021-06-01 17:39:55 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+
+app.set("views", path.join(__dirname, "public"));
+app.set("views engine", "ejs");
+
+
+
+
+app.get("/", (req, res) => {
+
+    res.render('user.ejs')
+
+})
+
+app.listen(3000, () => {
+    console.log('Server Running on 3000')
+})
