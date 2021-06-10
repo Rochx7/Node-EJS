ec908d3e7fa2797db79e9e0a071065578d040eb1
Guilherme Rocha <guirocha55@hotmail.com>
2021-06-10 18:12:57 -0300

    Adicionando arquivos ejs

--- script.js
+++ script.js
@@ -8,12 +8,33 @@ app.set("views", path.join(__dirname, "public"));
 app.set("views engine", "ejs");
 
 
-
+let users = [
+    {
+        id: 0,
+        name: 'Rocha',
+        phone: '(11)40028922'
+    },
+    {
+        id: 1,
+        name: 'Rocha',
+        phone: '(11)40028922'
+    },
+    {
+        id: 2,
+        name: 'Rocha',
+        phone: '(11)40028922'
+    }
+]
 
 app.get("/", (req, res) => {
 
-    res.render('user.ejs')
+    res.render('user.ejs', { users: users })
 
+})
+
+app.get("/about", (req, res) => {
+
+    res.render('about.ejs')
 })
 
 app.listen(3000, () => {
